a8a47d7b9479d1402ed447200082d6c95ad4a478
nclarke <nclarke>
2008-01-24 04:17:50 +0100

    some threadpool stuff iostream file dumper

---
+++ src/main/java/com/antiaction/mayhem/core/threadpool/ThreadPoolListener.java
@@ -0,0 +1,12 @@
+/*
+ * Created on 20/01/2008
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+package com.antiaction.mayhem.core.threadpool;
+
+public interface ThreadPoolListener {
+
+}
